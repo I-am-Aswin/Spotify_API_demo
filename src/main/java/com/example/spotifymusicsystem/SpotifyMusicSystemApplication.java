c8f7065f3a4be2014495ef6c53cbff7b217a2c53
I-am-Aswin <aswinsenthilkumar20@gmail.com>
2025-05-15 00:47:16 +0530

    Initial Project

---
+++ src/main/java/com/example/spotifymusicsystem/SpotifyMusicSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.spotifymusicsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpotifyMusicSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpotifyMusicSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/spotifymusicsystem/SpotifyMusicSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spotifymusicsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpotifyMusicSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
